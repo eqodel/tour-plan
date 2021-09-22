3d3df5da1e3bc1eea9f47952e4d514ea20b9a324
eqodel <eqodel@mail.ru>
2021-09-22 19:50:05 +0300

    create: added reviews section

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
-var swiper = new Swiper('.swiper', {
+var hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
   },
 });
 
-var swiper = new Swiper('.swiper', {
+var reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+});
+
+var hotelSlider = new Swiper('.hotel-slider', {
   keyboard: {
     enabled: true,
     onlyInViewport: false,
