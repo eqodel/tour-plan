8fc47083f6a030bba509eff4f191026ebcb341c8
eqodel <eqodel@mail.ru>
2021-10-02 00:53:42 +0300

    create: modal

--- js/main.js
+++ js/main.js
@@ -1,33 +1,61 @@
-var hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
+$(document).ready(function () {
+    var hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+  });
+
+  var reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+  });
+
+  var hotelSlider = new Swiper('.hotel-slider', {
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  }); 
+
+  var menuButton = document.querySelector(".menu-btn");
+  menuButton.addEventListener("click", function () {
+    document.querySelector(".navbar-menu").classList.toggle('navbar-menu--visible');
+  });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal);
+
+  function openModal() {
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.addClass('modal__overlay--visible');
+    modalDialog.addClass('modal__dialog--visible');
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  }
+
+  $(document).keydown(function(e) {
+  var code = e.keyCode || e.which;
+  if (code == 27) $(".modal").hide();
 });
 
-var reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-});
-
-var hotelSlider = new Swiper('.hotel-slider', {
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-}); 
-
-var menuButton = document.querySelector(".menu-btn");
-menuButton.addEventListener("click", function () {
-  document.querySelector(".navbar-menu").classList.toggle('navbar-menu--visible');
 });
